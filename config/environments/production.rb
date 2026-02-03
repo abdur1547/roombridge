fbe7212d91ceb6e7c7f00a0ad6791940ea9d88e3
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-02-04 00:55:19 +0500

    feat: don't log cnic and phone number in logs in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  # Filter sensitive parameters from logs
+  config.filter_parameters += [ :cnic, :phone_number ]
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
