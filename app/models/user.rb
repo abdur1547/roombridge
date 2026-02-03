d71531835be1bd3a415e6c185c9043ac8cc83a9a
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-02-04 01:08:48 +0500

    feat: cnic validation and global admin validation is added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,18 @@
 class User < ApplicationRecord
   enum :admin_verification_status, { unverified: 0, pending: 1, verified: 2, rejected: 3 }
   enum :gender, { male: 0, female: 1 }
-  enum :role, { seeker: 0, lister: 1, super_admin: 2 }
+  enum :role, { seeker: 0, lister: 1 }
 
   validates :phone_number, presence: true, uniqueness: true
+  validates :cnic, uniqueness: true, allow_nil: true
   validate :pakistani_phone_number
 
   def fully_verified?
     full_name.present? && cnic.present? && gender.present? && admin_verification_status.verified?
+  end
+
+  def super_admin?
+    ENV.fetch("GLOBAL_ADMIN_PHONE_NUMBERS", "").split(",").map(&:strip).include?(phone_number)
   end
 
   private
